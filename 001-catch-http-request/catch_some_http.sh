e9972cadfdd06e2e8ff1e6e38159491064176e79
fenghaoran <harrifeng@gmail.com>
2017-11-25 17:17:22 +0800

    may revert later

--- 001-catch-http-request/catch_some_http.sh
+++ 001-catch-http-request/catch_some_http.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-tcpdump -c 10000 -s 0 -i eth0 port 5000 -A | grep ":101,\|:302,\|:401,\|:701,\|:702" >> ret.log
+tcpdump -c 10000 -s 0 -i eth0 port 5000 -A | grep "cmd\":101,\|cmd\":302,\|cmd\":401,\|cmd\":701\|cmd\":702" >> ret.log
