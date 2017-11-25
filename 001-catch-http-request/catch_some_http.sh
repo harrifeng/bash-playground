a21cc427ee11aaebe7d850f15a3ceb73ab403518
fenghaoran <harrifeng@gmail.com>
2017-11-25 17:20:37 +0800

    remove 401

--- 001-catch-http-request/catch_some_http.sh
+++ 001-catch-http-request/catch_some_http.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-tcpdump -c 10000 -s 0 -i eth0 port 5000 -A | grep "cmd\":101,\|cmd\":302,\|cmd\":401,\|cmd\":701\|cmd\":702" >> ret.log
+tcpdump -c 1000000 -s 0 -i eth0 port 5000 -A | grep "cmd\":101,\|cmd\":302,\|cmd\":701\|cmd\":702" >> ret.log
