527bf670a500759aa6ff1a65b4eb90711e8f6b2f
Haoran Feng <harrifeng@gmail.com>
2017-11-27 07:37:54 +0000

    new network

--- 001-catch-http-request/get_by_cmd.sh
+++ 001-catch-http-request/get_by_cmd.sh
@@ -6,7 +6,7 @@ function get_cmd_num() {
     if [[ $(find $filename -type f -size +${minimumsize} 2>/dev/null) ]]; then
         $1 is already enough
     else
-        sudo tcpdump -c 10000 -s 0 -i eth0 port 5000 -A | grep "cmd\":${1}" >>  ${filename}
+        sudo tcpdump -c 10000 -s 0 -i enp3s0 port 5000 -A | grep "cmd\":${1}" >>  ${filename}
     fi
 }
 
