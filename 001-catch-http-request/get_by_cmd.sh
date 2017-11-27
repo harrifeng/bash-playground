edb0e4f29de2684685e58a6efd16472bde634131
Haoran Feng <harrifeng@gmail.com>
2017-11-27 07:55:24 +0000

    have a try

--- 001-catch-http-request/get_by_cmd.sh
+++ 001-catch-http-request/get_by_cmd.sh
@@ -4,16 +4,17 @@ function get_cmd_num() {
     minimumsize=10k
 
     if [[ $(find $filename -type f -size +${minimumsize} 2>/dev/null) ]]; then
-        $1 is already enough
+        echo "$1 is already enough--->"
     else
         sudo tcpdump -c 10000 -s 0 -i eth0 port 5000 -A | grep "cmd\":${1}" >>  ${filename}
     fi
 }
 
-for i in {1..100}
+for k in {1..100}
 do
-    declare -a cmd_list=("100" "200")
-    for i in "{$cmd_list[@]}"
+    declare -a arr=(100 200)
+
+    for i in "${arr[@]}"
     do
         get_cmd_num "$i"
     done
